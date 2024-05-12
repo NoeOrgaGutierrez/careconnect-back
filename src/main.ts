e26331bcae9920e4746a7a56da4afdb2153d1edb
Flateric2 <Pablofebe05@gmail.com>
2024-05-12 18:35:30 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap(): Promise<void> {
     .setVersion('1.0')
     .addTag('CareConnect API')
     .build()
+    app.enableCors()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
   app.useGlobalPipes(new ValidationPipe())
